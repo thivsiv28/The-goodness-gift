675438f3e73358c2fb3f7f06f1bb338509ebc5fc
Thiviya Sivasithamparam <thiviya.sivasithamparam@gmail.com>
2022-11-24 12:02:25 -0500

    auth.js/utils/server updated

---
+++ server/utils/auth.js
@@ -0,0 +1,32 @@
+const jwt = require("jsonwebtoken");
+
+const secret = "mysecretsshhhhh";
+const expiration = "2h";
+
+module.exports = {
+  authMiddleware: function ({ req }) {
+    let token = req.body.token || req.query.token || req.headers.authorization;
+
+    if (req.headers.authorization) {
+      token = token.split(" ").pop().trim();
+    }
+
+    if (!token) {
+      return req;
+    }
+
+    try {
+      const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      req.user = data;
+    } catch {
+      console.log("Invalid token");
+    }
+
+    return req;
+  },
+  signToken: function ({ username, email, _id }) {
+    const payload = { username, email, _id };
+
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
+};
