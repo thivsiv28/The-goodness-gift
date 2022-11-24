1ba27535ce01ba83c1a10a56f8ffefe0f898a6cb
Muaizkh <khanmu@my.yorku.ca>
2022-11-24 10:08:46 -0500

    updated Fundrasier.js

---
+++ server/models/Fundraiser.js
@@ -0,0 +1,30 @@
+const { Schema } = require("mongoose");
+
+const fundraiserSchema = new Schema({
+  poster: [
+    {
+      type: String,
+    },
+  ],
+  description: {
+    type: String,
+    required: true,
+  },
+  // saved fundrasier id from from site
+  fundrasierId: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+  },
+  link: {
+    type: String,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = fundraiserSchema;
