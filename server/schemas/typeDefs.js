29530cbde3275ca4401dc6d3d52e073d5edba4ec
Thiviya Sivasithamparam <thiviya.sivasithamparam@gmail.com>
2022-11-24 10:32:42 -0500

    type defs user and saved fundraiser

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,5 +1,22 @@
 const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
-  
+type SavedFundraiser {
+    poster: [String]
+    description: String!
+    fundraiserId: String!
+    image: String
+    link: String
+    title: String!
+}
+
+type User {
+    _id: ID!
+    username: String!
+    email: String!
+    password: String
+    savedFundraisers: [SavedFundraiser]
+
+    
+
 module.exports = typeDefs;
