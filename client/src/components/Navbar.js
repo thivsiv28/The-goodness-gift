d41d7beb80cf966e53b2227733ce98f3df59e23b
Muaizkh <khanmu@my.yorku.ca>
2022-11-27 21:14:03 -0500

    Navbar styling

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,73 +1,71 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { Navbar, Nav, Container, Modal, Tab } from 'react-bootstrap';
-import SignupForm from './SignupForm';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { Navbar, Nav, Container, Modal, Tab } from "react-bootstrap";
+import SignupForm from "./SignupForm";
 import LoginForm from "./LoginForm";
 
-import Auth from '../utils/auth';
+import Auth from "../utils/auth";
 
 const AppNavBar = () => {
 
-    const [showModal, setShowModal ] = useState(false);
+    const [showModal, setShowModal] = useState(false);
 
     return (
         <>
-        <Navbar bg= 'dark' variant= 'dark' expand='lg'>
+        <Navbar variant= "light" className="color-nav" expand="lg">
 <Container fluid>
 
-    <Navbar.Brand as={Link} to='/'>
-        The Goodness Gift 
+    <Navbar.Brand as={Link} to="/">
+        <img src= "/logo-no-background.png" width="64"/>
+        &nbsp; <span className="site-title"> The Goodness Gift </span>        
     </Navbar.Brand>
-    <Navbar.Toggle aria-controls='navbar'/>
-    <Navbar.Collapse id='navbar'>
-        <Nav className='ml-auto'>
-        <Nav.Link as={Link} to='/'>
-            Search for Fundraisers
-        </Nav.Link>
-
-        {Auth.loggedIn() ? (
-            <>
-            <Nav.Link as={Link} to='/saved'>
-                See your saved Fundraisers
-            </Nav.Link>
-            <Nav.Link onClick={Auth.logout}>Logout
-            </Nav.Link>
-            </>
-        ) : (
-
-            <Nav.Link onClick={()=> setShowModal(true)}
-            >Login/Sign Up</Nav.Link>
-        )}
+    <Navbar.Toggle aria-controls="navbar"/>
+    <Navbar.Collapse id="navbar">
+        <Nav className="ml-auto">
+            {Auth.loggedIn() ? (
+                <>
+                <Nav.Link as={Link} className="nav-text" to="/created">
+                    My Fundraisers
+                </Nav.Link>
+                <Nav.Link className="nav-text" onClick={Auth.logout}>
+                    Logout
+                </Nav.Link>
+                </>
+            ): (
+                <Nav.Link className="nav-text" onClick={() => setShowModal(true)}>
+                    Login/Sign Up
+                </Nav.Link>
+            )}
     </Nav>
     </Navbar.Collapse>
 </Container>
 </Navbar>
 
 <Modal
-size='lg'
+size="lg"
 show={showModal}
 onHide={() => setShowModal(false)}
-aria-labelledby='signup-modal'>
-<Tab.Container defaultActiveKey='login'>
+aria-labelledby="signup-modal">
+<Tab.Container defaultActiveKey="login">
     <Modal.Header closeButton>
-<Modal.Title id='signup-modal'>
-    <Nav variant='pills'>
+<Modal.Title id="signup-modal">
+    <Nav variant="pills">
         <Nav.Item>
-            <Nav.Link eventKey='login'>Login</Nav.Link>
+            <Nav.Link eventKey="login">Login</Nav.Link>
         </Nav.Item>
         <Nav.Item>
-            <Nav.Link eventKey='signup'>Sign Up</Nav.Link>
+            <Nav.Link eventKey="signup">Sign Up</Nav.Link>
         </Nav.Item>
     </Nav>
 </Modal.Title>
     </Modal.Header>
 <Modal.Body>
     <Tab.Content>
-<Tab.Pane eventKey='login'>
-    <LoginForm handleModalClose={()=> setShowModal(false)}/>
+<Tab.Pane eventKey="login">
+    <LoginForm handleModalClose={ () => setShowModal (false)}/>
 </Tab.Pane>
-<Tab.Pane eventKey='signup'>
-    <SignupForm handleModalClose={()=> setShowModal(false)}/>
+<Tab.Pane eventKey="signup">
+    <SignupForm handleModalClose={ () => setShowModal (false)}/>
 </Tab.Pane>
 </Tab.Content>
 </Modal.Body>
@@ -78,3 +76,6 @@ aria-labelledby='signup-modal'>
 };
 
 export default AppNavBar;
+
+
+
